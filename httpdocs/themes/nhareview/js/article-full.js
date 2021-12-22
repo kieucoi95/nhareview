b6782b7a611f1d24aa352745c51e1fdaa9211ab4
nghiem kieu <nghiemkieu95@gmail.com>
2021-12-22 23:11:03 +0700

    22/12/21

--- httpdocs/themes/nhareview/js/article-full.js
+++ httpdocs/themes/nhareview/js/article-full.js
@@ -1,13 +1,25 @@
 (function ($, Drupal, drupalSettings) {
     $(document).ready(function () {
         var swiper = new Swiper(".related_posts", {
-            slidesPerView: 3,
-            spaceBetween: 30,
+            slidesPerView: 1.3,
+            spaceBetween: 15,
             lazy: true,
             navigation: {
                 nextEl: ".slide_button .swiper-button-next",
                 prevEl: ".slide_button .swiper-button-prev",
             },
+            breakpoints: {
+                // when window width is >= 480px
+                480: {
+                    slidesPerView: 2,
+                    spaceBetween: 15
+                },
+                // when window width is >= 640px
+                992: {
+                    slidesPerView: 3,
+                    spaceBetween: 15
+                }
+            }
         });
     });
 })(jQuery, Drupal, drupalSettings);
--- httpdocs/themes/nhareview/js/script.js
+++ httpdocs/themes/nhareview/js/script.js
@@ -20,7 +20,17 @@
             }
         }, 500);
         
-        
+        $('.open_menu_mobile').click(function (e) { 
+            e.preventDefault();
+            $('.main_menu').addClass('active');
+            $('.bg_overlay').addClass('active');
+        });
+
+        $('.bg_overlay').click(function (e) { 
+            e.preventDefault();
+            $('.main_menu').removeClass('active');
+            $('.bg_overlay').removeClass('active');
+        });
     });
 })(jQuery, Drupal, drupalSettings);
 
